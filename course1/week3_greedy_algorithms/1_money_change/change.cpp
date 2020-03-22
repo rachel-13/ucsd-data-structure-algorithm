f56b907490dca98fd4d53749e1654b40d58af3c0
Rachel Fong <rach_pohyee13@hotmail.com>
2020-03-23 01:01:04 +0800

    Solved Course 1 - Week 3 - Q1 - Q2

--- course1/week3_greedy_algorithms/1_money_change/change.cpp
+++ course1/week3_greedy_algorithms/1_money_change/change.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
 
 int get_change(int m) {
-  //write your code here
-  return n;
+  
+  int one_denom = 1;
+  int five_denom = 5;
+  int ten_denom = 10;
+
+  int fives_unit = 0;
+  int tens_unit = 0;
+  int ones_unit = 0;
+
+  while(m > 0) {
+    if(m / ten_denom > 0) {
+      tens_unit = m / ten_denom;
+      m = m - (tens_unit * ten_denom);
+    }
+
+    if(m / five_denom > 0) {
+      fives_unit = m / five_denom;
+      m = m - (fives_unit * five_denom);
+    }
+
+    ones_unit = m; 
+    m = 0;
+  }
+
+  return tens_unit + fives_unit + ones_unit;
 }
 
 int main() {
--- course1/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
+++ course1/week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.cpp
@@ -1,14 +1,37 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>  
+#include <cmath>
 
 using std::vector;
 
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
-  double value = 0.0;
+  double total_value = 0.0000;
 
-  // write your code here
+  vector<double> value_per_weight(weights.size());
+  for (int i = 0; i < weights.size(); i++) {
+    value_per_weight[i] = (double) values[i] / (double) weights[i];
+  }
+   
+  std::vector<double>::iterator max_value_weight;
+  int max_value_weight_index = -1;
 
-  return value;
+  while(capacity > 0) {
+    max_value_weight = max_element(value_per_weight.begin(), value_per_weight.end());
+    max_value_weight_index = distance(value_per_weight.begin(), max_value_weight);
+
+    if(weights[max_value_weight_index] <= capacity) {
+      total_value = total_value + (double)(weights[max_value_weight_index] * *max_value_weight);
+      capacity = capacity - weights[max_value_weight_index];
+    } else {
+      total_value = total_value + (double)(capacity * *max_value_weight);
+      capacity = 0;
+    }
+
+    value_per_weight[max_value_weight_index] = 0;
+  }
+
+  return total_value;
 }
 
 int main() {
@@ -23,7 +46,6 @@ int main() {
 
   double optimal_value = get_optimal_value(capacity, weights, values);
 
-  std::cout.precision(10);
-  std::cout << optimal_value << std::endl;
+  printf("%.4f\n", optimal_value);
   return 0;
 }
