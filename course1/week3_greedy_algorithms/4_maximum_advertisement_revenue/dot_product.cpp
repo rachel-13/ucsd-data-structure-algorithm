3b88b94219b765023adb02e3fcfa2a4850b87d3e
Rachel Fong <rach_pohyee13@hotmail.com>
2020-03-26 01:44:50 +0800

    Solved Course 1 - Week 3 - Q4

--- course1/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
+++ course1/week3_greedy_algorithms/4_maximum_advertisement_revenue/dot_product.cpp
@@ -7,6 +7,8 @@ using std::vector;
 long long max_dot_product(vector<int> a, vector<int> b) {
   // write your code here
   long long result = 0;
+  sort(a.begin(), a.end());
+  sort(b.begin(), b.end());
   for (size_t i = 0; i < a.size(); i++) {
     result += ((long long) a[i]) * b[i];
   }
