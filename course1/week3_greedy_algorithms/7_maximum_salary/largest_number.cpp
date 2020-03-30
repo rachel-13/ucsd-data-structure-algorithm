d0c2c9c57959e3b4b9607327ca817d2d9de4c1df
Rachel Fong <rach_pohyee13@hotmail.com>
2020-03-30 16:29:01 +0800

    Resolved Course 1 - Week 3 - Q7

--- course1/week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
+++ course1/week3_greedy_algorithms/7_maximum_salary/largest_number.cpp
@@ -7,12 +7,47 @@
 using std::vector;
 using std::string;
 
-string largest_number(vector<string> a) {
+bool isGreaterOrEqual(string a, string b) {
+  string ab = a + b;
+  string ba = b + a;
+
+  int ab_int; 
+  std::stringstream ab_s(ab);
+  ab_s >> ab_int;
+
+  int ba_int; 
+  std::stringstream ba_s(ba);
+  ba_s >> ba_int;
+
+  if(ab_int > ba_int) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+string largest_number(vector<string> digits) {
   //write your code here
   std::stringstream ret;
-  for (size_t i = 0; i < a.size(); i++) {
-    ret << a[i];
+  while(digits.size() > 0) {
+    string maxDigit = "0";
+
+    for (size_t i = 0; i < digits.size(); i++) {
+      string digit = digits[i];
+      if(isGreaterOrEqual(digit, maxDigit)) {
+        maxDigit = digit;
+      }
+    }
+
+    ret << maxDigit;
+
+    // removing max digit string
+    std::vector<string>::iterator position = std::find(digits.begin(), digits.end(), maxDigit);
+    if (position != digits.end()) {
+      digits.erase(position);
+    }
   }
+
   string result;
   ret >> result;
   return result;
