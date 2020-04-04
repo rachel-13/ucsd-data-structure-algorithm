2883cb181c6c866a0d51f656925b8ba077ef01a1
Rachel Fong <rach_pohyee13@hotmail.com>
2020-04-05 01:59:42 +0800

    Resolved Course 1 - Week 4 - Q5

--- course1/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
+++ course1/week4_divide_and_conquer/5_organizing_a_lottery/points_and_segments.cpp
@@ -1,11 +1,67 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
+using std::pair;
+using std::make_pair;
+
+vector<int> findIndexForElement(int point, vector<int> points) {
+  vector<int> result;
+
+  for(int j = 0; j < points.size(); j++) {
+    if (points[j] == point) {
+      result.push_back(j);
+    }
+  }
+
+  return result;
+}
 
 vector<int> fast_count_segments(vector<int> starts, vector<int> ends, vector<int> points) {
   vector<int> cnt(points.size());
-  //write your code here
+  vector< pair<int, char> > mapOfSegmentsAndPoints;
+
+  for(int i = 0; i < starts.size(); i++) {
+    mapOfSegmentsAndPoints.push_back(make_pair(starts[i], 'l'));
+    mapOfSegmentsAndPoints.push_back(make_pair(ends[i], 'r'));
+  }
+
+  for(int i = 0; i < points.size(); i++) {
+    mapOfSegmentsAndPoints.push_back(make_pair(points[i], 'p'));
+  }
+
+  sort(mapOfSegmentsAndPoints.begin(), mapOfSegmentsAndPoints.end());
+
+  int index = 0;
+  int openSegmentCount = 0;
+  while(index < mapOfSegmentsAndPoints.size()) {
+    
+    pair<int, char> currentPair = mapOfSegmentsAndPoints[index];
+
+    if (currentPair.second == 'l') {
+      openSegmentCount++;
+    } else if (currentPair.second == 'r') {
+      openSegmentCount--;
+    }
+
+    if(currentPair.second == 'p' && openSegmentCount > 0) {
+      vector<int> pointsIndices = findIndexForElement(currentPair.first, points);
+      for(int i = 0; i < pointsIndices.size(); i++) {
+        int originalPointIndex = pointsIndices[i];
+        cnt[originalPointIndex] = openSegmentCount;
+      }
+    } else if(currentPair.second == 'p' && openSegmentCount <= 0) {
+      vector<int> pointsIndices = findIndexForElement(currentPair.first, points);
+      for(int i = 0; i < pointsIndices.size(); i++) {
+        int originalPointIndex = pointsIndices[i];
+        cnt[originalPointIndex] = 0;
+      }
+    }
+
+    index++;
+  }
+
   return cnt;
 }
 
@@ -19,7 +75,73 @@ vector<int> naive_count_segments(vector<int> starts, vector<int> ends, vector<in
   return cnt;
 }
 
+void test_solution() {
+  while(true) {
+    int segmentInputSize = rand() % 10 + 1;
+    int pointInputSize = rand() % 10 + 1;
+    std::cout << segmentInputSize << " " << pointInputSize << "\n";
+
+    vector<int> starts(segmentInputSize), ends(segmentInputSize);
+    for(int i = 0; i < segmentInputSize; i++) {
+      int start = (rand() % 10) - (rand() % 10);
+      int end = start + (rand() % 10 + 1);
+      starts[i] = start;
+      ends[i] = end;
+      std::cout << start << " " << end << "\n";
+    }
+
+    vector<int> points(pointInputSize);
+    for(int i = 0; i < pointInputSize; i++) {
+      int point = (rand() % 10) - (rand() % 10);
+      points[i] = point;
+      std::cout << point << " ";
+    }  
+
+    vector<int> res1 = naive_count_segments(starts, ends, points);
+    vector<int> res2 = fast_count_segments(starts, ends, points);
+
+    if(res1 == res2) {
+      std::cout << "\n\nOK!\n";
+      for(int i = 0; i < pointInputSize; i++) {
+        std::cout << res1[i] << " ";
+      }
+      std::cout << "\nvs\n";
+      for(int i = 0; i < pointInputSize; i++) {
+        std::cout << res2[i] << " ";
+      }
+
+      std::cout << "\n==========================\n";
+    } else {
+
+      std::cout << "\n\nWrong result \n";
+      for(int i = 0; i < pointInputSize; i++) {
+        std::cout << res1[i] << " ";
+      }
+      std::cout << "\nvs\n";
+      for(int i = 0; i < pointInputSize; i++) {
+        std::cout << res2[i] << " ";
+      }
+      std::cout << "\n==========================\n";
+      break;
+    }
+  }
+}
+
+void test_one_solution() {
+  vector<int> starts = {5, -3, 7}; 
+  vector<int> ends = {13, 7, 13};
+  vector<int> points = {1, -5, -3, 8};
+
+  vector<int> res = naive_count_segments(starts, ends, points);
+  for(int i = 0; i < res.size(); i++) {
+    std::cout << res[i] << " ";
+  }
+}
+
 int main() {
+  // test_solution();
+  // test_one_solution();
+
   int n, m;
   std::cin >> n >> m;
   vector<int> starts(n), ends(n);
@@ -31,7 +153,7 @@ int main() {
     std::cin >> points[i];
   }
   //use fast_count_segments
-  vector<int> cnt = naive_count_segments(starts, ends, points);
+  vector<int> cnt = fast_count_segments(starts, ends, points);
   for (size_t i = 0; i < cnt.size(); i++) {
     std::cout << cnt[i] << ' ';
   }
