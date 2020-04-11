fe2deb15a46abbc0683e4ea360d89032394aee6d
Rachel Fong <rach_pohyee13@hotmail.com>
2020-04-11 22:42:49 +0800

    Resolved Course 1 - Week 5 - Q1

--- course1/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
+++ course1/week5_dynamic_programming1/1_money_change_again/change_dp.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <cmath>
 
 int get_change(int m) {
-  //write your code here
-  return m / 4;
+  
+  int money = m;
+  int minNumCoins[money + 1];
+  minNumCoins[0] = 0;
+
+  int coins[3] = { 1, 3, 4 };
+
+  for(int m = 1; m <= money; m++) {
+    minNumCoins[m] = INFINITY;
+    for(int i = 0; i <= 2; i++) {
+      if (m >= coins[i]) {
+        int currentNumCoin = minNumCoins[m - coins[i]] + 1;
+        if(currentNumCoin < minNumCoins[m]) {
+          minNumCoins[m] = currentNumCoin;
+        }
+      }
+    }
+  }
+  
+  return minNumCoins[money];
 }
 
 int main() {
