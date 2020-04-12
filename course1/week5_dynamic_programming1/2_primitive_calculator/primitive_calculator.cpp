66fb8326fe87cffc8767087ecc3f6cf7751f32e5
Rachel Fong <rach_pohyee13@hotmail.com>
2020-04-12 16:31:44 +0800

    Resolved Course 1 - Week 5 - Q2

--- course1/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
+++ course1/week5_dynamic_programming1/2_primitive_calculator/primitive_calculator.cpp
@@ -6,16 +6,33 @@ using std::vector;
 
 vector<int> optimal_sequence(int n) {
   std::vector<int> sequence;
-  while (n >= 1) {
-    sequence.push_back(n);
-    if (n % 3 == 0) {
-      n /= 3;
-    } else if (n % 2 == 0) {
-      n /= 2;
-    } else {
-      n = n - 1;
+  std::vector<int> cache(n+1);
+  cache[0] = 0;
+  cache[1] = 0;
+
+  // building a cache of how many operations it takes to get to 2,3,4...n
+  for(int i = 2; i <= n; i++) {
+    cache[i] = cache[i - 1] + 1;
+
+    if(i % 3 == 0) {
+      cache[i] = std::min(cache[i / 3] + 1, cache[i]);
+    } else if(i % 2 == 0) {
+      cache[i] = std::min(cache[i / 2] + 1, cache[i]);
     }
   }
+
+  for (int i = n; i > 1; ) {
+      sequence.push_back(i);
+      
+      if (i % 3 == 0 && (cache[i / 3] == cache[i] - 1))
+        i = i / 3; 
+      else if (i % 2 == 0 && (cache[i / 2] == cache[i] - 1))
+        i = i / 2;
+      else if (cache[i - 1] == cache[i] - 1)
+        i = i - 1;
+  }
+  sequence.push_back(1);
+
   reverse(sequence.begin(), sequence.end());
   return sequence;
 }
