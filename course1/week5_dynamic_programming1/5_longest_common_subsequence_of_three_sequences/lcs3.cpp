b465670c8a6b9a2339099e970528fb337f4daaf4
Rachel Fong <rach_pohyee13@hotmail.com>
2020-04-16 09:29:24 +0800

    Resolved Course 1 - Week 5 - Q5

--- course1/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.cpp
+++ course1/week5_dynamic_programming1/5_longest_common_subsequence_of_three_sequences/lcs3.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
 
 using std::vector;
 
-int lcs3(vector<int> &a, vector<int> &b, vector<int> &c) {
-  //write your code here
-  return std::min(std::min(a.size(), b.size()), c.size());
+int lcs3(vector<int> &aArr, const int &aSize, vector<int> &bArr, const int &bSize, vector<int> &cArr, const int &cSize) {
+  const int row = aSize + 1;
+  const int col = bSize + 1;
+  const int depth = cSize + 1;
+  int table[row][col][depth];
+  memset(&table[0][0][0], 0, sizeof(table));
+
+  for(int c = 1; c < col; c++) {
+    for(int r = 1; r < row; r++) {
+      for(int d = 1; d < depth; d++) {
+        if(aArr[r - 1] == bArr[c - 1] && aArr[r - 1]== cArr[d - 1] ) {
+          table[r][c][d] = table[r-1][c-1][d-1] + 1;
+        } else {
+          table[r][c][d] = std::max(table[r-1][c][d], std::max(table[r][c-1][d], table[r][c][d-1]));
+        } 
+      }
+    }
+  }
+
+  int result = table[aSize][bSize][cSize];
+  return result;
 }
 
 int main() {
@@ -27,5 +46,5 @@ int main() {
   for (size_t i = 0; i < cn; i++) {
     std::cin >> c[i];
   }
-  std::cout << lcs3(a, b, c) << std::endl;
+  std::cout << lcs3(a, a.size(), b, b.size(), c, c.size()) << std::endl;
 }
