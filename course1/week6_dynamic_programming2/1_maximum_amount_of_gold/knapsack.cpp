3a4ab0297cae7e71130ecb0fc845e2a73051d4d2
Rachel Fong <rach_pohyee13@hotmail.com>
2020-04-18 23:16:52 +0800

    Resolved Course 1 - Week 6 - Q1

--- course1/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
+++ course1/week6_dynamic_programming2/1_maximum_amount_of_gold/knapsack.cpp
@@ -3,15 +3,37 @@
 
 using std::vector;
 
-int optimal_weight(int W, const vector<int> &w) {
-  //write your code here
-  int current_weight = 0;
-  for (size_t i = 0; i < w.size(); ++i) {
-    if (current_weight + w[i] <= W) {
-      current_weight += w[i];
+int optimal_weight(const int &W, const vector<int> &w, const int &wSize) {
+  
+  const int row = wSize + 1;
+  const int col = W + 1;
+  vector < vector<int> > table(row, vector<int>(col));
+
+  for(int i = 0; i < row; i++) {
+    table[i][0] = 0;
+  }
+
+  for(int j = 0; j < col; j++) {
+    table[0][j] = 0;
+  }
+
+  for(int r = 1; r < row; r++) {
+    for(int c = 1; c < col; c++) {
+      table[r][c] = table[r - 1][c];
+
+      int currentItemWeight = w[r - 1];
+      if (currentItemWeight <= c) {
+        int newWeight = table[r - 1][c - currentItemWeight] + currentItemWeight;
+        if (table[r][c] < newWeight) {
+          table[r][c] = newWeight;
+        }
+      }
+      
     }
   }
-  return current_weight;
+
+  int result = table[wSize][W];
+  return result;
 }
 
 int main() {
@@ -21,5 +43,5 @@ int main() {
   for (int i = 0; i < n; i++) {
     std::cin >> w[i];
   }
-  std::cout << optimal_weight(W, w) << '\n';
+  std::cout << optimal_weight(W, w, w.size()) << '\n';
 }
