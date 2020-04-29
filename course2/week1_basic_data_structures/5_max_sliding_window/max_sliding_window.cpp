db9f9aa55e0a7e5104a9e7ca43b44c477df67cf4
Rachel Fong <rach_pohyee13@hotmail.com>
2020-04-30 01:08:03 +0800

    Resolved Course 1 - Week 1 - Q5

--- course2/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.cpp
+++ course2/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <vector>
 #include <deque>
-#include <algorithm>  
+#include <algorithm>
 
 using std::cin;
 using std::cout;
-using std::vector;
 using std::max;
+using std::vector;
 
-void max_sliding_window_naive(vector<int> const & A, int w) {
-    for (size_t i = 0; i < A.size() - w + 1; ++i) {
+void max_sliding_window_naive(vector<int> const &A, int w)
+{
+    for (size_t i = 0; i < A.size() - w + 1; ++i)
+    {
         int window_max = A.at(i);
         for (size_t j = i + 1; j < i + w; ++j)
             window_max = max(window_max, A.at(j));
@@ -20,51 +22,61 @@ void max_sliding_window_naive(vector<int> const & A, int w) {
     return;
 }
 
-void max_sliding_window(vector<int> const & A, int w) {
+void max_sliding_window(vector<int> const &A, int w)
+{
 
-    if(A.size() == w) {
-        cout<< *max_element(A.begin(), A.end());
+    if (A.size() == w)
+    {
+        cout << *max_element(A.begin(), A.end());
         return;
     }
 
     std::deque<int> q;
-    int max = -1;
-    int index = 0;
 
-    while(index < A.size()) {
-        
-       if(q.size() < w) 
-       {  
-           q.push_back(A[index]);
-       } 
-       else 
-       {
-            q.pop_front();
-            q.push_back(A[index]);
-       }
-
-       index++;
-
-       if(index >= w) 
-       {
-           max = *max_element(q.begin(), q.end());
-           cout << max << " ";
-       }
+    // find the first window
+    for(int i = 0; i < w; i++ ) {
+        // remove all indexes of smaller elements than the current element
+       while(!q.empty() && A[i] >= A[q.back()]) {
+            q.pop_back();
+        }
+        q.push_back(i);
     }
+
+    for(int k = w; k < A.size(); k++)
+    {
+        cout << A[q.front()] << " ";
+
+        // remove all indexes of elements that have moved out of the window
+        while(!q.empty() && q.front() <= k - w) {
+            q.pop_front();
+        }
+
+        // remove all indexes of smaller elements than the current element
+        while(!q.empty() && A[k] >= A[q.back()]) {
+            q.pop_back();
+        }
+
+        q.push_back(k);
+    }
+
+    cout << A[q.front()];
 
     return;
 }
 
-void test_solution() {
+void test_solution()
+{
     vector<int> A;
-    for(int i = 0; i <= 100000; i++) {
+    for (int i = 0; i <= 100000; i++)
+    {
         A.push_back(0);
     }
 
     max_sliding_window(A, 1);
 }
 
-int main() {
+int main()
+{
     // test_solution();
 
     int n = 0;
