6c63d5d8eb5ca2da80ebcdf300cd5894df18cbb9
Rachel Fong <rach_pohyee13@hotmail.com>
2020-05-17 23:26:34 +0800

    Unoptimal solution Course 2 - Week 4 - Q3

--- course2/week4_hash_tables/3_hash_substring/hash_substring.cpp
+++ course2/week4_hash_tables/3_hash_substring/hash_substring.cpp
@@ -1,28 +1,59 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <math.h>
 
+using std::cin;
 using std::string;
 typedef unsigned long long ull;
 
-struct Data {
+struct Data
+{
     string pattern, text;
 };
 
-Data read_input() {
+long long hash_string(const string &S)
+{
+    // hashing formula
+    // sum from i = 0 to S.length() - 1
+    // S[i] * (x ^ i) mod p
+    long long result = 0;
+    long long L = pow(10, 8); // maximum length of data to hash, in this question it is 8
+    long long p = L + 19;     // prime number bigger than 10 ^ L
+    long long x = 31;         // random number 1 < x < p - 1
+
+    for (int i = S.length() - 1; i >= 0; i--)
+    {
+        result = (result * x + S[i]) % p;
+    }
+
+    return result;
+}
+
+Data read_input()
+{
+    // std::fstream file("./tests/03");
+    // if (file.is_open())
+    // {
+    //     cin.rdbuf(file.rdbuf());
+    // }
+
     Data data;
     std::cin >> data.pattern >> data.text;
     return data;
 }
 
-void print_occurrences(const std::vector<int>& output) {
+void print_occurrences(const std::vector<int> &output)
+{
     for (size_t i = 0; i < output.size(); ++i)
         std::cout << output[i] << " ";
     std::cout << "\n";
 }
 
-std::vector<int> get_occurrences(const Data& input) {
-    const string& s = input.pattern, t = input.text;
+std::vector<int> get_occurrences_naive(const Data &input)
+{
+    const string &s = input.pattern, t = input.text;
     std::vector<int> ans;
     for (size_t i = 0; i + s.size() <= t.size(); ++i)
         if (t.substr(i, s.size()) == s)
@@ -30,8 +61,67 @@ std::vector<int> get_occurrences(const Data& input) {
     return ans;
 }
 
+bool are_equal(string s1, string s2)
+{
+    if (s1.length() != s2.length())
+    {
+        return false;
+    }
 
-int main() {
+    for (int i = 0; i < s1.length() - 1; i++)
+    {
+        if (s1[i] != s2[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+std::vector<int> get_occurrences(const Data &input)
+{
+    const string &s = input.pattern, t = input.text;
+    std::vector<int> ans;
+
+    long long patternHash = hash_string(s);
+
+    int textPatternLengthDiff = t.length() - s.length();
+    string textToCompare = "";
+    int startIndex = 0;
+
+    for (int i = 0; i <= textPatternLengthDiff; i++)
+    {
+        int endIndex = i + s.length() - 1;
+        if (textToCompare.length() == s.length())
+        {
+            textToCompare.erase(0, 1);
+            textToCompare = textToCompare + t[endIndex];
+        }
+        else
+        {
+            textToCompare = t.substr(startIndex, endIndex + 1);
+        }
+
+        startIndex = startIndex + 1;
+
+        long long textHash = hash_string(textToCompare);
+        if (textHash != patternHash)
+        {
+            continue;
+        }
+
+        if (are_equal(textToCompare, s))
+        {
+            ans.push_back(i);
+        }
+    }
+
+    return ans;
+}
+
+int main()
+{
     std::ios_base::sync_with_stdio(false);
     print_occurrences(get_occurrences(read_input()));
     return 0;
