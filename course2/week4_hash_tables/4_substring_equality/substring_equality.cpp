e4a1173b8edba83f91e57458361eb312f4af0578
Rachel Fong <rach_pohyee13@hotmail.com>
2020-05-24 17:17:53 +0800

    Resolved Course 2 - Week 4 - Q4

--- course2/week4_hash_tables/4_substring_equality/substring_equality.cpp
+++ course2/week4_hash_tables/4_substring_equality/substring_equality.cpp
@@ -13,6 +13,8 @@ class Solver
 public:
 	vector<long long> h1;
 	vector<long long> h2;
+	vector<long long> x_pow_l_m1;
+	vector<long long> x_pow_l_m2;
 	int m1 = pow(10, 9) + 7;
 	int m2 = pow(10, 9) + 9;
 	int x = 31;
@@ -26,39 +28,63 @@ public:
 		h1[0] = 0;
 		h2[0] = 0;
 
+		precompute_power_for_length(s.length());
 		for (int i = 1; i <= s.length(); i++)
 		{
 			// formula: ℎ1[𝑖] ← (𝑥 · ℎ1[𝑖 − 1] + s𝑖) mod 𝑚1
-			h1[i] = (x * h1[i - 1] + s[i - 1]) % m1;
-			h2[i] = (x * h2[i - 1] + s[i - 1]) % m2;
+			long long x_h1 = ((x % m1) * (h1[i - 1] % m1)) % m1;
+			h1[i] = (x_h1 + s[i - 1]) % m1;
+
+			long long x_h2 = ((x % m2) * (h2[i - 1] % m2)) % m2;
+			h2[i] = (x_h2 + s[i - 1]) % m2;
+		}
+	}
+
+	void precompute_power_for_length(int length)
+	{
+		long long value_m1 = 1;
+		long long value_m2 = 1;
+		
+		x_pow_l_m1.clear();
+		x_pow_l_m2.clear();
+		
+		x_pow_l_m1.push_back(1);
+		x_pow_l_m2.push_back(1);
+
+		while (length > 0)
+		{
+			value_m1 *= x;
+			value_m1 %= m1;
+			x_pow_l_m1.push_back(value_m1);
+
+			value_m2 *= x;
+			value_m2 %= m2;
+			x_pow_l_m2.push_back(value_m2);
+
+			length--;
 		}
 	}
 
 	bool ask(int a, int b, int l)
 	{
 		// formula: 𝐻(s𝑎s𝑎+1 · · · s𝑎+𝑙−1) = ℎ[𝑎 + 𝑙] − (𝑥^𝑙)ℎ[𝑎]
-		long long x_pow_l = pow(x, l);
 
 		// calculate for mod m1
-		long long substringHash_a_m1 = h1[a + l];
-		long long a_m1 = h1[a];
-		long long x_pow_l_a_hash_m1 = (x_pow_l * a_m1) % m1;
+		long long substringHash_a_m1 = h1[a + l] % m1;
+		long long x_pow_l_a_hash_m1 = (x_pow_l_m1[l] * (h1[a] % m1)) % m1;
 		long long hash_a_m1 = (((substringHash_a_m1 - x_pow_l_a_hash_m1) % m1) + m1) % m1;
 
-		long long substringHash_b_m1 = h1[b + l];
-		long long b_m1 = h1[b];
-		long long x_pow_l_b_hash_m1 = (x_pow_l * b_m1) % m1;
+		long long substringHash_b_m1 = h1[b + l] % m1;
+		long long x_pow_l_b_hash_m1 = (x_pow_l_m1[l] * (h1[b] % m1)) % m1;
 		long long hash_b_m1 = (((substringHash_b_m1 - x_pow_l_b_hash_m1) % m1) + m1) % m1;
 
 		// calculate for mod m2
-		long long substringHash_a_m2 = h2[a + l];
-		long long a_m2 = h2[a];
-		long long x_pow_l_a_hash_m2 = (x_pow_l * a_m2) % m2;
+		long long substringHash_a_m2 = h2[a + l] % m2;
+		long long x_pow_l_a_hash_m2 = (x_pow_l_m2[l] * (h2[a] % m2)) % m2;
 		long long hash_a_m2 = (((substringHash_a_m2 - x_pow_l_a_hash_m2) % m2) + m2) % m2;
 
-		long long substringHash_b_m2 = h2[b + l];
-		long long b_m2 = h2[b];
-		long long x_pow_l_b_hash_m2 = (x_pow_l * b_m2) % m2;
+		long long substringHash_b_m2 = h2[b + l] % m2;
+		long long x_pow_l_b_hash_m2 = (x_pow_l_m2[l] * (h2[b] % m2)) % m2;
 		long long hash_b_m2 = (((substringHash_b_m2 - x_pow_l_b_hash_m2) % m2) + m2) % m2;
 
 		return hash_a_m1 == hash_b_m1 && hash_a_m2 == hash_b_m2;
@@ -77,7 +103,7 @@ string printstring(int n)
 						'y', 'z'};
 	string ran = "";
 	for (int i = 0; i < n; i++)
-		ran = ran + letters[rand() % 3];
+		ran = ran + letters[rand() % 26];
 	return ran;
 }
 
@@ -85,40 +111,44 @@ void test_solution()
 {
 	while (true)
 	{
-		int startIndex1 = rand() % 10;
-		int startIndex2 = rand() % 10;
-		int textLength = rand() % (max(startIndex1, startIndex2)) + 10;
-		string text =  printstring(textLength);
-		int length = textLength - max(startIndex1, startIndex2);
-		
+		int queryCount = rand() % 10;
+		int textLength = rand() % 50000 + 5;
+		string text = printstring(textLength);
 
 		Solver solver(text);
 
-		bool res1 = solver.ask_naive(startIndex1, startIndex2, length);
-		bool res2 = solver.ask(startIndex1, startIndex2, length);
-
-		if (res1 == res2)
+		for (int i = 0; i < queryCount; i++)
 		{
-			cout << "\n====================================================\n";
-			cout << "\nOK!\n";
+			int startIndex1 = rand() % textLength;
+			int startIndex2 = rand() % textLength;
+			int length = textLength - max(startIndex1, startIndex2);
 
-			cout << res1 << " vs " << res2 << "\n";
+			bool res1 = solver.ask_naive(startIndex1, startIndex2, length);
+			bool res2 = solver.ask(startIndex1, startIndex2, length);
 
-			cout << "\n"
-				 << text << "\n";
-			cout << startIndex1 << " " << startIndex2 << " " << length << "\n";
-		}
-		else
-		{
-			cout << "\n====================================================\n";
-			cout << "\nFail!\n";
+			if (res1 == res2)
+			{
+				cout << "\n====================================================\n";
+				cout << "\nOK!\n";
 
-			cout << res1 << " vs " << res2 << "\n";
+				cout << res1 << " vs " << res2 << "\n";
 
-			cout << "\n"
-				 << text << "\n";
-			cout << startIndex1 << " " << startIndex2 << " " << length << "\n";
-			break;
+				cout << "\n"
+					 << text << "\n";
+				cout << startIndex1 << " " << startIndex2 << " " << length << "\n";
+			}
+			else
+			{
+				cout << "\n====================================================\n";
+				cout << "\nFail!\n";
+
+				cout << res1 << " vs " << res2 << "\n";
+
+				cout << "\n"
+					 << text << "\n";
+				cout << startIndex1 << " " << startIndex2 << " " << length << "\n";
+				break;
+			}
 		}
 	}
 }
@@ -127,7 +157,7 @@ int main()
 {
 	ios_base::sync_with_stdio(0), cin.tie(0);
 
-	fstream cin("./tests/02");
+	// fstream cin("./tests/05");
 	// test_solution();
 
 	string s;
