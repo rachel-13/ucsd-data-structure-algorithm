7e69a600addb86866c37e68dd5cd09c9e314391b
Rachel Fong <rach_pohyee13@hotmail.com>
2020-06-03 23:32:54 +0800

    Suboptimal solution Course 2 - Week 6 - Q1

--- course2/week6_binary_search_trees/1_tree_traversals/tree-orders.cpp
+++ course2/week6_binary_search_trees/1_tree_traversals/tree-orders.cpp
@@ -1,69 +1,176 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <fstream>
+#include <stack>
 #if defined(__unix__) || defined(__APPLE__)
 #include <sys/resource.h>
 #endif
 
-using std::vector;
-using std::ios_base;
 using std::cin;
 using std::cout;
+using std::find;
+using std::ios_base;
+using std::stack;
+using std::vector;
 
-class TreeOrders {
+class TreeOrders
+{
   int n;
-  vector <int> key;
-  vector <int> left;
-  vector <int> right;
+  vector<int> key;
+  vector<int> left;
+  vector<int> right;
 
 public:
-  void read() {
+  void read()
+  {
+    // std::fstream cin("./tests/21");
+
     cin >> n;
     key.resize(n);
     left.resize(n);
     right.resize(n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++)
+    {
       cin >> key[i] >> left[i] >> right[i];
     }
   }
 
-
-  vector <int> in_order() {
+  vector<int> in_order()
+  {
     vector<int> result;
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
+    stack<int> stack;
+    vector<int> visitedNodeIndices;
+
+    int rootIndex = 0;
+    stack.push(rootIndex);
+    visitedNodeIndices.push_back(rootIndex);
+
+    while (result.size() < key.size())
+    {
+      if (left[rootIndex] != -1 &&
+          find(visitedNodeIndices.begin(), visitedNodeIndices.end(), left[rootIndex]) == visitedNodeIndices.end())
+      {
+        int leftChildIndex = left[rootIndex];
+        stack.push(leftChildIndex);
+        rootIndex = leftChildIndex;
+        continue;
+      }
+
+      rootIndex = stack.top();
+      stack.pop();
+      result.push_back(key[rootIndex]);
+      visitedNodeIndices.push_back(rootIndex);
+
+      if (right[rootIndex] != -1 &&
+          find(visitedNodeIndices.begin(), visitedNodeIndices.end(), right[rootIndex]) == visitedNodeIndices.end())
+      {
+        int rightChildIndex = right[rootIndex];
+        stack.push(rightChildIndex);
+        rootIndex = rightChildIndex;
+        continue;
+      }
+    }
 
     return result;
   }
 
-  vector <int> pre_order() {
-    vector<int> result;    
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
-    
+  vector<int> pre_order()
+  {
+    vector<int> result;
+    vector<int> visitedNodeIndices;
+    stack<int> stack;
+
+    int rootIndex = 0;
+    result.push_back(key[rootIndex]);
+    visitedNodeIndices.push_back(rootIndex);
+    stack.push(rootIndex);
+    while (result.size() < key.size())
+    {
+      if (left[rootIndex] != -1 &&
+          find(visitedNodeIndices.begin(), visitedNodeIndices.end(), left[rootIndex]) == visitedNodeIndices.end())
+      {
+        int leftChildIndex = left[rootIndex];
+        result.push_back(key[leftChildIndex]);
+        visitedNodeIndices.push_back(leftChildIndex);
+        stack.push(leftChildIndex);
+        rootIndex = leftChildIndex;
+        continue;
+      }
+
+      if (right[rootIndex] != -1 &&
+          find(visitedNodeIndices.begin(), visitedNodeIndices.end(), right[rootIndex]) == visitedNodeIndices.end())
+      {
+        int rightChildIndex = right[rootIndex];
+        result.push_back(key[rightChildIndex]);
+        visitedNodeIndices.push_back(rightChildIndex);
+        stack.push(rightChildIndex);
+        rootIndex = rightChildIndex;
+        continue;
+      }
+
+      rootIndex = stack.top();
+      stack.pop();
+    }
+
     return result;
   }
 
-  vector <int> post_order() {
+  vector<int> post_order()
+  {
     vector<int> result;
-    // Finish the implementation
-    // You may need to add a new recursive method to do that
-    
+    stack<int> stack;
+    vector<int> visitedNodeIndices;
+
+    int rootIndex = 0;
+    stack.push(rootIndex);
+    visitedNodeIndices.push_back(rootIndex);
+
+    while (result.size() < key.size())
+    {
+      rootIndex = stack.top();
+
+      if (left[rootIndex] != -1 &&
+          find(visitedNodeIndices.begin(), visitedNodeIndices.end(), left[rootIndex]) == visitedNodeIndices.end())
+      {
+        int leftChildIndex = left[rootIndex];
+        stack.push(leftChildIndex);
+        continue;
+      }
+
+      if (right[rootIndex] != -1 &&
+          find(visitedNodeIndices.begin(), visitedNodeIndices.end(), right[rootIndex]) == visitedNodeIndices.end())
+      {
+        int rightChildIndex = right[rootIndex];
+        stack.push(rightChildIndex);
+        continue;
+      }
+
+      stack.pop();
+
+      result.push_back(key[rootIndex]);
+      visitedNodeIndices.push_back(rootIndex);
+    }
+
     return result;
   }
 };
 
-void print(vector <int> a) {
-  for (size_t i = 0; i < a.size(); i++) {
-    if (i > 0) {
+void print(vector<int> a)
+{
+  for (size_t i = 0; i < a.size(); i++)
+  {
+    if (i > 0)
+    {
       cout << ' ';
     }
     cout << a[i];
   }
-  cout << '\n';
+  cout << "\n";
 }
 
-int main_with_large_stack_space() {
+int main_with_large_stack_space()
+{
   ios_base::sync_with_stdio(0);
   TreeOrders t;
   t.read();
@@ -73,29 +180,28 @@ int main_with_large_stack_space() {
   return 0;
 }
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 #if defined(__unix__) || defined(__APPLE__)
   // Allow larger stack space
-  const rlim_t kStackSize = 16 * 1024 * 1024;   // min stack size = 16 MB
+  const rlim_t kStackSize = 16 * 1024 * 1024; // min stack size = 16 MB
   struct rlimit rl;
   int result;
 
   result = getrlimit(RLIMIT_STACK, &rl);
   if (result == 0)
   {
-      if (rl.rlim_cur < kStackSize)
+    if (rl.rlim_cur < kStackSize)
+    {
+      rl.rlim_cur = kStackSize;
+      result = setrlimit(RLIMIT_STACK, &rl);
+      if (result != 0)
       {
-          rl.rlim_cur = kStackSize;
-          result = setrlimit(RLIMIT_STACK, &rl);
-          if (result != 0)
-          {
-              std::cerr << "setrlimit returned result = " << result << std::endl;
-          }
+        std::cerr << "setrlimit returned result = " << result << "\n";
       }
+    }
   }
 #endif
 
   return main_with_large_stack_space();
 }
-
