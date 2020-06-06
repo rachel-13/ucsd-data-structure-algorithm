b0cb1df2eb3a70ae7530ba94c25dce0cca96dcbc
Rachel Fong <rach_pohyee13@hotmail.com>
2020-06-07 00:20:02 +0800

    Resolved Course 2 - Week 6 - Q2

--- course2/week6_binary_search_trees/2_is_bst/is_bst.cpp
+++ course2/week6_binary_search_trees/2_is_bst/is_bst.cpp
@@ -1,13 +1,17 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <stack>
 
 using std::cin;
 using std::cout;
 using std::endl;
+using std::stack;
 using std::vector;
 
-struct Node {
+struct Node
+{
   int key;
   int left;
   int right;
@@ -16,23 +20,74 @@ struct Node {
   Node(int key_, int left_, int right_) : key(key_), left(left_), right(right_) {}
 };
 
-bool IsBinarySearchTree(const vector<Node>& tree) {
-  // Implement correct algorithm here
-  return true;
+bool is_sorted_by_key( const Node & lhs, const Node & rhs )
+{
+   return lhs.key < rhs.key;
 }
 
-int main() {
+bool IsBinarySearchTree(const vector<Node> &tree)
+{
+  // note to self: a binary search tree is a sorted binary tree and from the previous question (tree_traversal.cpp) is it seen that in-order traversal 
+  // printed out the node keys in a sorted order. hence we just need to do an in-order traversal and check if the results are sorted.
+  if (tree.size() == 0)
+    return true;
+
+  stack<Node> stackOfNodes;
+  stack<int> stackOfIndices;
+
+  Node root = tree[0];
+  int rootIndex = 0;
+  vector<Node> result;
+
+  while (rootIndex != -1 || !stackOfNodes.empty())
+  {
+    if (rootIndex != -1)
+    {
+      stackOfNodes.push(root);
+      stackOfIndices.push(rootIndex);
+
+      Node leftChild = tree[root.left];
+      rootIndex = root.left;
+      root = leftChild;
+    }
+    else
+    {
+      root = stackOfNodes.top();
+      rootIndex = stackOfIndices.top();
+      stackOfNodes.pop();
+      stackOfIndices.pop();
+
+      result.push_back(tree[rootIndex]);
+
+      Node rightChild = tree[root.right];
+      rootIndex = root.right;
+      root = rightChild;
+    }
+  }
+
+  bool resultIsSorted = std::is_sorted(result.begin(), result.end(), is_sorted_by_key);
+  return resultIsSorted;
+}
+
+int main()
+{
+  // std::fstream cin("./tests/06");
+
   int nodes;
   cin >> nodes;
   vector<Node> tree;
-  for (int i = 0; i < nodes; ++i) {
+  for (int i = 0; i < nodes; ++i)
+  {
     int key, left, right;
     cin >> key >> left >> right;
     tree.push_back(Node(key, left, right));
   }
-  if (IsBinarySearchTree(tree) {
+  if (IsBinarySearchTree(tree))
+  {
     cout << "CORRECT" << endl;
-  } else {
+  }
+  else
+  {
     cout << "INCORRECT" << endl;
   }
   return 0;
