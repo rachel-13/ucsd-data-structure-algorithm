04b2ef4d31736512117e973741dc76bcde59d029
Rachel Fong <rach_pohyee13@hotmail.com>
2020-07-03 01:01:40 +0800

    Implemented Phase 1 Simplex Course 5 - Week 2 - Q2

--- course5/week2_linear_programming/ad_allocation/ad_allocation.cpp
+++ course5/week2_linear_programming/ad_allocation/ad_allocation.cpp
@@ -2,55 +2,282 @@
 #include <iostream>
 #include <vector>
 #include <cstdio>
+#include <fstream>
 using namespace std;
 
 typedef vector<vector<double>> matrix;
 
-pair<int, vector<double>> allocate_ads(
-    int n, 
-    int m, 
-    matrix A, 
-    vector<double> b, 
-    vector<double> c) {
+struct Position
+{
+public:
+  int row, column;
+};
 
-  // Write your code here
-
-  return {0, vector<double>(m, 0)};
+void printTableu(matrix tableu)
+{
+  for (int i = 0; i < tableu.size(); i++)
+  {
+    for (int j = 0; j < tableu[0].size(); j++)
+    {
+      cout << setw(10) << tableu[i][j] << " ";
+    }
+    cout << "\n";
+  }
 }
 
-int main(){
+void printVector(vector<double> v)
+{
+  for (int i = 0; i < v.size(); i++)
+  {
+    cout << setw(10) << v[i] << " ";
+  }
+  cout << "\n";
+}
+
+void buildPhase1Tableu(int n, int m, matrix &tableu, matrix A, vector<double> b, vector<double> c, int numOfAritificals, vector<double> &w, vector<int> &basicVarIndex)
+{
+  // build decision variables
+  for (int i = 0; i < tableu.size(); i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
+      if (i == tableu.size() - 1)
+      {
+        tableu[i][j] = c[j] * -1;
+      }
+      else
+      {
+        if (b[i] < 0)
+        {
+          tableu[i][j] = A[i][j] * -1;
+        }
+        else
+        {
+          tableu[i][j] = A[i][j];
+        }
+      }
+    }
+  }
+
+  // build last column (a.k.a Answer Column)
+  int lastColIndex = tableu[0].size() - 1;
+  for (int i = 0; i < tableu.size(); i++)
+  {
+    if (i == tableu.size() - 1)
+    {
+      tableu[i][lastColIndex] = 0;
+    }
+    else
+    {
+      if (b[i] < 0)
+      {
+        tableu[i][lastColIndex] = b[i] * -1;
+      }
+      else
+      {
+        tableu[i][lastColIndex] = b[i];
+      }
+    }
+  }
+
+  // build slack and aritificial variables
+  vector<Position> artificialVarsIndex;
+  for (int i = 0; i < tableu.size(); i++)
+  {
+    // exclude last row which stores the original objective function
+    if (i == tableu.size() - 1)
+    {
+      break;
+    }
+
+    for (int j = m; j < tableu.size() - 1; j++)
+    {
+      if (b[i] < 0)
+      {
+        tableu[i][i + j] = -1;
+        int artificalVarColIndex = lastColIndex - numOfAritificals;
+        tableu[i][artificalVarColIndex] = 1;
+        numOfAritificals--;
+        artificialVarsIndex.push_back({i, artificalVarColIndex});
+        basicVarIndex[i] = artificalVarColIndex;
+      }
+      else
+      {
+        tableu[i][i + j] = 1;
+        basicVarIndex[i] = i + j;
+      }
+    }
+  }
+
+  // build artifical objective row
+  for (int i = 0; i < artificialVarsIndex.size(); i++)
+  {
+    for (int j = 0; j < tableu[0].size(); j++)
+    {
+      Position pos = artificialVarsIndex[i];
+      if (pos.column != j)
+      {
+        w[j] = w[j] + tableu[pos.row][j];
+      }
+    }
+  }
+}
+
+int simplexPhase1(matrix &tableu, vector<double> &w, vector<int> &basicVarIndex)
+{
+  int result = 0;
+
+  while (true)
+  {
+    // choose pivot column, look for the most positive int in artificial row
+    int pivotColumn = -1;
+    int pivotColumnValue = -1;
+    for (int i = 0; i < w.size() - 1 - 1; i++) // last column a.k.a Answer Column doesn't count
+    {
+      if (w[i] > pivotColumnValue && w[i] > 0)
+      {
+        pivotColumn = i;
+        pivotColumnValue = w[i];
+      }
+    }
+
+    // choose pivot row
+    int pivotRow = -1;
+    int lastColIndex = tableu[0].size() - 1;
+    double ratio = INT_MAX;
+    for (int i = 0; i < tableu.size() - 1; i++) // last row doesn't count because it stores the original objective, hence another - 1 expression
+    {
+      if (tableu[i][pivotColumn] >= 0)
+      {
+        double rowPivotRatio = tableu[i][lastColIndex] / tableu[i][pivotColumn];
+        if (rowPivotRatio < ratio)
+        {
+          pivotRow = i;
+          ratio = rowPivotRatio;
+        }
+      }
+    }
+
+    if(pivotColumn == -1 || pivotRow == -1)
+    {
+      break;
+    }
+
+    // pivot phase 1
+    double tableuPivotPointCoeff = tableu[pivotRow][pivotColumn];
+    if (tableuPivotPointCoeff != 1)
+    {
+      for (int i = 0; i < tableu[0].size(); i++)
+      {
+        tableu[pivotRow][i] = tableu[pivotRow][i] / tableuPivotPointCoeff;
+      }
+    }
+
+    for (int i = 0; i < tableu.size(); i++)
+    {
+      if (i == pivotRow)
+      {
+        continue;
+      }
+      double rowToClearCoeff = tableu[i][pivotColumn];
+      double artificialRowToClearCoeff = w[pivotColumn];
+
+      for (int j = 0; j < tableu[0].size(); j++)
+      {
+        tableu[i][j] = tableu[i][j] - (rowToClearCoeff * tableu[pivotRow][j]);
+        w[j] = w[j] - (artificialRowToClearCoeff * tableu[pivotRow][j]);
+      }
+    }
+    cout<<"\n=====================\n";
+    printTableu(tableu);
+    printVector(w);
+  }
+
+  return result;
+}
+
+pair<int, vector<double>> allocate_ads(
+    int n,
+    int m,
+    matrix A,
+    vector<double> b,
+    vector<double> c)
+{
+  int solution = 0;
+  vector<double> result(m, 0);
+
+  int tableuPhase1Row = n + 1;
+  int tableuPhase1Col = b.size() * 2; // 1 column for every constraint
+  int numberOfArtificials = 0;
+
+  for (int i = 0; i < b.size(); i++)
+  {
+    if (b[i] < 0)
+    {
+      tableuPhase1Col = tableuPhase1Col + 1; // adding a new column for every artifical variable for >= constraint
+      numberOfArtificials++;
+    }
+  }
+
+  matrix tableuPhase1(tableuPhase1Row, vector<double>(tableuPhase1Col, 0));
+  vector<double> w(tableuPhase1[0].size(), 0); // aritifical objective function w
+  vector<int> basicVarIndex(n);
+  buildPhase1Tableu(n, m, tableuPhase1, A, b, c, numberOfArtificials, w, basicVarIndex);
+  printTableu(tableuPhase1);
+  printVector(w);
+  cout<<"\n=====================\n";
+  simplexPhase1(tableuPhase1, w, basicVarIndex);
+
+  return {solution, result};
+}
+
+int main()
+{
+  std::fstream file("./tests/own_test_1");
+
+  if (file.is_open())
+  {
+    cin.rdbuf(file.rdbuf());
+  }
+
   int n, m;
   cin >> n >> m;
   matrix A(n, vector<double>(m));
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
       cin >> A[i][j];
     }
   }
   vector<double> b(n);
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     cin >> b[i];
   }
   vector<double> c(m);
-  for (int i = 0; i < m; i++) {
+  for (int i = 0; i < m; i++)
+  {
     cin >> c[i];
   }
 
   pair<int, vector<double>> ans = allocate_ads(n, m, A, b, c);
 
-  switch (ans.first) {
-    case -1: 
-      printf("No solution\n");
-      break;
-    case 0: 
-      printf("Bounded solution\n");
-      for (int i = 0; i < m; i++) {
-        printf("%.18f%c", ans.second[i], " \n"[i + 1 == m]);
-      }
-      break;
-    case 1:
-      printf("Infinity\n");
-      break;      
+  switch (ans.first)
+  {
+  case -1:
+    printf("No solution\n");
+    break;
+  case 0:
+    printf("Bounded solution\n");
+    for (int i = 0; i < m; i++)
+    {
+      printf("%.18f%c", ans.second[i], " \n"[i + 1 == m]);
+    }
+    break;
+  case 1:
+    printf("Infinity\n");
+    break;
   }
   return 0;
 }
--- course5/week2_linear_programming/diet/diet.cpp
+++ course5/week2_linear_programming/diet/diet.cpp
@@ -2,55 +2,73 @@
 #include <iostream>
 #include <vector>
 #include <cstdio>
+#include <fstream>
+
 using namespace std;
+using std::vector;
 
 typedef vector<vector<double>> matrix;
 
 pair<int, vector<double>> solve_diet_problem(
-    int n, 
-    int m, 
-    matrix A, 
-    vector<double> b, 
-    vector<double> c) {
+    int n,
+    int m,
+    matrix A,
+    vector<double> b,
+    vector<double> c)
+{
+  int solution = 0;
+  vector<double> result(m, 0);
 
-  // Write your code here
-
-  return {0, vector<double>(m, 0)};
+  return {0, result};
 }
 
-int main(){
+int main()
+{
+  std::fstream file("./tests/01");
+
+  if (file.is_open())
+  {
+    cin.rdbuf(file.rdbuf());
+  }
+
   int n, m;
   cin >> n >> m;
   matrix A(n, vector<double>(m));
-  for (int i = 0; i < n; i++) {
-    for (int j = 0; j < m; j++) {
+  for (int i = 0; i < n; i++)
+  {
+    for (int j = 0; j < m; j++)
+    {
       cin >> A[i][j];
     }
   }
   vector<double> b(n);
-  for (int i = 0; i < n; i++) {
+  for (int i = 0; i < n; i++)
+  {
     cin >> b[i];
   }
   vector<double> c(m);
-  for (int i = 0; i < m; i++) {
+  for (int i = 0; i < m; i++)
+  {
     cin >> c[i];
   }
 
   pair<int, vector<double>> ans = solve_diet_problem(n, m, A, b, c);
 
-  switch (ans.first) {
-    case -1: 
-      printf("No solution\n");
-      break;
-    case 0: 
-      printf("Bounded solution\n");
-      for (int i = 0; i < m; i++) {
-        printf("%.18f%c", ans.second[i], " \n"[i + 1 == m]);
-      }
-      break;
-    case 1:
-      printf("Infinity\n");
-      break;      
+  switch (ans.first)
+  {
+  case -1:
+    printf("No solution\n");
+    break;
+  case 0:
+    printf("Bounded solution\n");
+    for (int i = 0; i < m; i++)
+    {
+      printf("%.18f%c", ans.second[i], " \n"[i + 1 == m]);
+    }
+    break;
+  case 1:
+    printf("Infinity\n");
+    break;
   }
   return 0;
 }
