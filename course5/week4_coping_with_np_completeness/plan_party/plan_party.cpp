df960fcaa606846a4d4b00076485eb6f4d8d9b63
Rachel Fong <rach_pohyee13@hotmail.com>
2020-07-24 17:37:54 +0800

    Resolved Course 5 - Week 4 - Q2

--- course5/week4_coping_with_np_completeness/plan_party/plan_party.cpp
+++ course5/week4_coping_with_np_completeness/plan_party/plan_party.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
-struct Vertex {
+using namespace std;
+struct Vertex
+{
     int weight;
-    std::vector <int> children;
+    std::vector<int> children;
 };
 typedef std::vector<Vertex> Graph;
 typedef std::vector<int> Sum;
 
-Graph ReadTree() {
+Graph ReadTree()
+{
     int vertices_count;
+
+    // std::fstream file("./tests/03");
+
+    // if (file.is_open())
+    // {
+    //     std::cin.rdbuf(file.rdbuf());
+    // }
     std::cin >> vertices_count;
 
     Graph tree(vertices_count);
@@ -17,7 +28,8 @@ Graph ReadTree() {
     for (int i = 0; i < vertices_count; ++i)
         std::cin >> tree[i].weight;
 
-    for (int i = 1; i < vertices_count; ++i) {
+    for (int i = 1; i < vertices_count; ++i)
+    {
         int from, to, weight;
         std::cin >> from >> to;
         tree[from - 1].children.push_back(to - 1);
@@ -27,46 +39,67 @@ Graph ReadTree() {
     return tree;
 }
 
-void dfs(const Graph &tree, int vertex, int parent) {
-    for (int child : tree[vertex].children)
-        if (child != parent)
-            dfs(tree, child, vertex);
-
-    // This is a template function for processing a tree using depth-first search.
-    // Write your code here.
-    // You may need to add more parameters to this function for child processing.
-}
-
-int MaxWeightIndependentTreeSubset(const Graph &tree) {
-    size_t size = tree.size();
-    if (size == 0)
-        return 0;
-    dfs(tree, 0, -1);
-    // You must decide what to return.
-    return 0;
-}
-
-int main() {
-    // This code is here to increase the stack size to avoid stack overflow
-    // in depth-first search.
-    const rlim_t kStackSize = 64L * 1024L * 1024L;  // min stack size = 64 Mb
-    struct rlimit rl;
-    int result;
-    result = getrlimit(RLIMIT_STACK, &rl);
-    if (result == 0)
+int dfs(const Graph &tree, int vertex, int parent, vector<int> &maxWeightArr, vector<bool> &visitedNodes)
+{
+    if (maxWeightArr[vertex] == -1)
     {
-        if (rl.rlim_cur < kStackSize)
+        if (tree[vertex].children.size() == 0)
         {
-            rl.rlim_cur = kStackSize;
-            result = setrlimit(RLIMIT_STACK, &rl);
-            if (result != 0)
+            maxWeightArr[vertex] = tree[vertex].weight;
+        }
+        else
+        {
+            int m1 = tree[vertex].weight;
+            for (int i = 0; i < tree[vertex].children.size(); i++)
             {
-                fprintf(stderr, "setrlimit returned result = %d\n", result);
+                int child = tree[vertex].children[i];
+                if(child == parent)
+                {
+                    continue;
+                }
+                for (int grandchild : tree[child].children)
+                {
+                    if(grandchild != vertex)
+                    {
+                        m1 = m1 + dfs(tree, grandchild, child, maxWeightArr, visitedNodes);
+                    }
+                }
             }
+
+            int m0 = 0;
+            for (int i = 0; i < tree[vertex].children.size(); i++)
+            {   
+                int child = tree[vertex].children[i];
+                if(child == parent)
+                {
+                    continue;
+                }
+                m0 = m0 + dfs(tree, child, vertex, maxWeightArr, visitedNodes);
+            }
+
+            maxWeightArr[vertex] = max(m0,m1);
         }
     }
 
-    // Here begins the solution
+    return maxWeightArr[vertex];
+}
+
+int MaxWeightIndependentTreeSubset(const Graph &tree)
+{
+    size_t size = tree.size();
+    if (size == 0)
+    {
+        return 0;
+    }
+
+    vector<int> maxWeightArr(tree.size(), -1);
+    vector<bool> visitedNodes(tree.size(), false);
+
+    return dfs(tree, 0, -1, maxWeightArr, visitedNodes);;
+}
+
+int main()
+{
     Graph tree = ReadTree();
     int weight = MaxWeightIndependentTreeSubset(tree);
     std::cout << weight << std::endl;
